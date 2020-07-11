78e06e63fbc3fe2d1880ac68cac0517a52c4edec
Geocarlos Alves <geocarlos.alves@pixida.com.br>
2020-07-11 20:37:04 -0300

    Fix detail page

--- src/containers/Word.jsx
+++ src/containers/Word.jsx
@@ -4,17 +4,19 @@ import AppContext from '../context/app-context';
 
 const Word = () => {
 	const history = useHistory();
-	const { wordList, audioList } = useContext(AppContext);
+	const { wordList, audioUrl } = useContext(AppContext);
 
 	const { wordId } = useParams();
 
+	const keys = Object.keys(wordList);
+
 	return (
 		<div style={{ margin: '10%' }}>
-			<h1>{wordList[Object.keys(wordList)[wordId]].entry}</h1>
+			<h1>{wordList[keys[wordId]].entry}</h1>
 			<audio controls>
-				<source src={audioList[wordId].audio}/>
+				<source src={`${audioUrl + wordList[keys[wordId]].audios[0]}?alt=media`}/>
 			</audio>
-			{wordList[Object.keys(wordList)[wordId]].definitions.map((d, i)=> (
+			{wordList[keys[wordId]].definitions.map((d, i)=> (
 				<React.Fragment key={d.definition}>
 					<p>{`${i + 1}. ${d.definition}`}</p>
 					{d.examples.map(e => (
